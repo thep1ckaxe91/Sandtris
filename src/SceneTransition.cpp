7ef1694cad357d4b6a8129ea982c6e3d1735068c
thep1ckaxe91 <ThePickaxe91@gmail.com>
2025-12-31 20:42:42 +0700

    reset: I literally forget what i'm  focusing on, its performance, not how to migrate stuff. So remove all, back to SDL2

--- src/SceneTransition.cpp
+++
@@ -1,8 +0,0 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-
-#include "SceneTransition.hpp"
-
-
-SceneTransition::SceneTransition(double time){this->time = time; this->isDone = 0;}
-SceneTransition::~SceneTransition() = default;
